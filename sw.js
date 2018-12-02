1a8989b7f2fe7ed367e2ae9844a3b0cae628c67e
Mateusz Kawka <kaawkamateusz@gmail.com>
2018-12-02 13:39:36 +0100

    Initial dist subtree commit

--- sw.js
+++ sw.js
@@ -2,6 +2,10 @@ importScripts('/quiz-app-nuxt/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
+    "url": "/quiz-app-nuxt/_nuxt/208001378ffd0e6f2a0b.js",
+    "revision": "16b3e762e9a8af31cb34c4ed090e377a"
+  },
+  {
     "url": "/quiz-app-nuxt/_nuxt/4d58220fb2b8632ea98f.js",
     "revision": "0079841d7e96d75f3c67a4d122761b99"
   },
@@ -18,16 +22,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "ec37424a92d5123b01ad92fe42d104d9"
   },
   {
-    "url": "/quiz-app-nuxt/_nuxt/b935c0ca1ef5c164f6d1.js",
-    "revision": "6b1d6d1eebcc20867e765fa743682955"
+    "url": "/quiz-app-nuxt/_nuxt/b568814a835700034bbd.js",
+    "revision": "4f8c8ea5f60ab36b2065186fb51f460d"
   },
   {
-    "url": "/quiz-app-nuxt/_nuxt/c0951e3b6705a1d4d9dd.js",
-    "revision": "56fc992440dbdf8b020d785048b2bc67"
-  },
-  {
-    "url": "/quiz-app-nuxt/_nuxt/f0ba3b82ca104a9ff1f6.js",
-    "revision": "8bf5f00c41dcca5119531ad0ef514cdd"
+    "url": "/quiz-app-nuxt/_nuxt/da0f991594cc1e7e5591.js",
+    "revision": "37e1f559c7b5611082774cd0a8b4a7db"
   }
 ], {
   "cacheId": "quiz-app-v2",
